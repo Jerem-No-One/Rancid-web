a41628a9e7f33947ff21404f078f80c2db51cbae
jeremy person <jeremy person>
2017-03-13 15:08:45 +0100

    changes

--- code.js
+++ code.js
@@ -3,7 +3,8 @@ function passwordEnable()
   if(document.getElementById('oui').checked == true)
   {
     document.getElementById('password_enable').style.display = "none";
-  }else if (document.getElementById('non').checked == true)
+  }
+  else if (document.getElementById('non').checked == true)
   {
     document.getElementById('password_enable').style.display = "block";
   }
